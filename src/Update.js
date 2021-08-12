88e24b9a5181a27b4e96faf9d1e7a7eb0fb262c1
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-08-12 04:28:14 +0300

    lab14

--- src/Update.js
+++ src/Update.js
@@ -1,49 +1,24 @@
-import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from 'react'
 
-class Update extends Component {
-
+class UpdateCatForm extends Component {
     render() {
         return (
-            <div>
-              <br />
-              
-              <h3>information</h3>
+            <form onSubmit={(e) => this.props.updateBook(e)}>
+                <fieldset>
+                    <legend>Update book Info</legend>
+                    <label>book Name</label>
+                    <input type="text" name="bookName" defaultValue={this.props.bookName} />
+                    <label>book Description</label>
+                    <input type="text" name="bookDescription" defaultValue
+                        ={this.props.bookDescription} />
 
-              <br />
-              <br />
-
-              <Form style={{ width: '20rem' }}>
-                <Form.Group>
-                  <Form.Label>Book Name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter Book Name" 
-                  onChange={(e) => {this.props.updateBookName(e)}} value={this.props.bookName}
-                  />
-                </Form.Group>
-                
-                <Form.Group>
-                  <Form.Label>Book Status</Form.Label>
-                  <Form.Control type="text" placeholder="Password" 
-                  onChange={(e) => {this.props.updatebookStatus(e)}} value={this.props.bookStatus}
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                  <Form.Label>Book Description</Form.Label>
-                  <Form.Control as="textarea" rows={3} 
-                   onChange={(e) => {this.props.updateBookDescription(e)}} value={this.props.bookDescription}
-                  />
-                </Form.Group>
-                <Button onClick={this.props.updateBook}>Submit Update</Button>
-              </Form>
-             
-                <br />
-                <br />
-
-            </div>
+                    <label>book Status</label>
+                    <input type="text" name="bookStatus" defaultValue={this.props.bookStatus} />
+                    <input type="submit" value="Update" />
+                </fieldset>
+            </form>
         )
     }
 }
 
-export default Update;
+export default UpdateCatForm;
